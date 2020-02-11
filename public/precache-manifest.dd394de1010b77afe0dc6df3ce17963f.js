2ea5dd9a03b35f25c59a65ce5882ed4226a7aef8
Anarghya V <30490462+gitofanarghya@users.noreply.github.com>
2020-02-12 03:21:37 +0530

    adding built react app code

---
+++ public/precache-manifest.dd394de1010b77afe0dc6df3ce17963f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46825e8ed4de678c290a39e2e4ee5009",
+    "url": "/index.html"
+  },
+  {
+    "revision": "879d8fbe674275f459b9",
+    "url": "/static/css/2.cbc96f7a.chunk.css"
+  },
+  {
+    "revision": "124f9580d444e574234a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "879d8fbe674275f459b9",
+    "url": "/static/js/2.16846c5d.chunk.js"
+  },
+  {
+    "revision": "22b436cca9c8a3225f78c0659850a8dc",
+    "url": "/static/js/2.16846c5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "124f9580d444e574234a",
+    "url": "/static/js/main.d3043976.chunk.js"
+  },
+  {
+    "revision": "0c9b1b6fff7fcb81ba8b",
+    "url": "/static/js/runtime-main.43d3ec00.js"
+  }
+]);
