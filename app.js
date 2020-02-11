7ad4666a55f9dabfc7712a7640182f8ecdc4f020
Anarghya V <30490462+gitofanarghya@users.noreply.github.com>
2020-02-12 03:08:31 +0530

    cors off

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ var softNotificationsRouter = require('./routes/softNotifications');
 var residentRouter = require('./routes/resident')
 var motionDataRouter = require('./routes/motionData')
 var orgRouter = require('./routes/org')
-var cors = require('cors')
+//var cors = require('cors')
 var app = express();
-app.use(cors())
+//app.use(cors())
 
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
