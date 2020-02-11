4f9779597d70c9381920de934719a200db3710d3
Anarghya V <30490462+gitofanarghya@users.noreply.github.com>
2020-02-12 03:27:51 +0530

    UI changes

---
+++ public/precache-manifest.77a12ebe08d982cd20adbeed557ad3f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea04f02d5e2b27589b75ed5eb3fe117",
+    "url": "/index.html"
+  },
+  {
+    "revision": "879d8fbe674275f459b9",
+    "url": "/static/css/2.cbc96f7a.chunk.css"
+  },
+  {
+    "revision": "120375e54e6b6eed6f31",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "879d8fbe674275f459b9",
+    "url": "/static/js/2.16846c5d.chunk.js"
+  },
+  {
+    "revision": "22b436cca9c8a3225f78c0659850a8dc",
+    "url": "/static/js/2.16846c5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "120375e54e6b6eed6f31",
+    "url": "/static/js/main.c6581629.chunk.js"
+  },
+  {
+    "revision": "0c9b1b6fff7fcb81ba8b",
+    "url": "/static/js/runtime-main.43d3ec00.js"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dd394de1010b77afe0dc6df3ce17963f.js"
+  "/precache-manifest.77a12ebe08d982cd20adbeed557ad3f8.js"
 );
 
 self.addEventListener('message', (event) => {
